9190b17accbc9dc75f99f3303b9aa8c0dc7f6c59
Malo <malo.forrest@gmail.com>
2025-05-13 23:44:03 +0200

    Ajout d'un gestionnaire d'erreurs de remplissage du formulaire de bindings

--- serverweb/static/scripts/treeview.js
+++ serverweb/static/scripts/treeview.js
@@ -292,7 +292,8 @@ function createTree(account, bindingIndex) {
 
 function selectObject (bindingIndex, account, force = false) {
   if (selectedElementUI && selectedElementObj || force){
-    bindings[bindingIndex].netilion_binding_id = selectedElementObj.id;
+    updateBinding(bindingIndex, "netilion_binding_id", selectedElementObj.id, true)
+    // bindings[bindingIndex].netilion_binding_id = selectedElementObj.id;
 
     if (account.assets.includes(selectedElementObj)) {
       document.getElementById(`binding-label-${bindingIndex}`).innerHTML = `
