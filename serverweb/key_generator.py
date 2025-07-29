fade8877d12ea179c4cac90807568181c806f76f
Malo <malo.forrest@gmail.com>
2025-07-29 13:54:53 +0200

    Harmonisation des noms de variable d'environnement
    Signed-off-by: Malo <malo.forrest@gmail.com>

--- serverweb/key_generator.py
+++ serverweb/key_generator.py
@@ -3,5 +3,5 @@ import base64, os
 def generate_key():
     return base64.urlsafe_b64encode(os.urandom(32)).decode()
 
-print(f'FLASK_SESSION_KEY ="{generate_key()}"')
+print(f'FLASK_SECRET_KEY ="{generate_key()}"')
 print(f'CONFIG_ENCRYPTION_KEY ="{generate_key()}"')
